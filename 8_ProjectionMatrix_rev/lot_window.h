a7bb02f1ece65ee7fa23e049211b01a4aad2b392
kimyuheon <67135427+kimyuheon@users.noreply.github.com>
2025-09-04 17:50:33 +0900

    Add files via upload

---
+++ 8_ProjectionMatrix_rev/lot_window.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <string>
+
+namespace lot {
+    class LotWindow {
+        public:
+            LotWindow(int w, int h, std::string name);
+            ~LotWindow();
+
+            LotWindow(const LotWindow &) = delete;
+            LotWindow &operator=(const LotWindow &) = delete;
+
+            bool shouldClose() { return glfwWindowShouldClose(window); }
+
+            VkExtent2D getExtent() {
+                return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
+            }
+            bool wasWindowReSized() { return framebufferResized; }
+            void resetWindowResizedFlag() { framebufferResized = false; }
+
+            void createWindowSurface(VkInstance instance, VkSurfaceKHR *surfacec_);
+
+            bool isUserResizing() const { return userResizing; }   // getter (public)
+            void setUserResizing(bool v) { userResizing = v; }     // setter (public)
+
+        private:
+            static void framebufferResizeCallback(GLFWwindow *window, int width, int height);
+            void initWindow();
+
+            int width;
+            int height;
+            bool framebufferResized = false;
+
+            std::string windowName;
+            GLFWwindow *window;
+
+            bool userResizing = false;
+    };
+}
