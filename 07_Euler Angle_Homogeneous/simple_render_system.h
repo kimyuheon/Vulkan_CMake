4c2757c2785b73b1b178b4e58315d27fd43ea48a
kimyuheon <67135427+kimyuheon@users.noreply.github.com>
2025-09-05 13:55:11 +0900

    Add files via upload

---
+++ 07_Euler Angle_Homogeneous/simple_render_system.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "lot_device.h"
+#include "lot_game_object.h"
+#include "lot_pipeline.h"
+
+// std
+#include <memory>
+#include <vector>
+
+namespace lot {
+    class SimpleRenderSystem {
+        public:
+            SimpleRenderSystem(LotDevice &device, VkRenderPass renderPass);
+            ~SimpleRenderSystem();
+
+            SimpleRenderSystem(const SimpleRenderSystem &) = delete;
+            SimpleRenderSystem& operator=(const SimpleRenderSystem &) = delete;
+
+            void renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LotGameObject> &gameObjects);
+        private:
+            void createPipelineLayout();
+            void createPipeline(VkRenderPass renderPass);
+
+            LotDevice& lotDevice;
+
+            std::unique_ptr<LotPipeline> lotPipeline;
+            VkPipelineLayout pipelineLayout;
+    };
+}
