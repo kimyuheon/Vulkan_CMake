906430bec9761afc324071890bc114919f61d094
kimyuheon <67135427+kimyuheon@users.noreply.github.com>
2025-09-05 13:53:46 +0900

    Add files via upload

---
+++ 02_frag_vert_Apply/first_app.cpp
@@ -0,0 +1,121 @@
+#include "first_app.h"
+
+#include <array>
+#include <stdexcept>
+#include <iostream>
+
+namespace lot {
+    FirstApp::FirstApp() {
+        loadModels();
+        createPipelineLayout();
+        createPipeline();
+        createCommandBuffers();
+    }
+
+    FirstApp::~FirstApp() {
+        vkDestroyPipelineLayout(lotDevice.device(), pipelineLayout, nullptr);
+    }
+
+    void FirstApp::run() {
+        while (!lotWindow.shouldClose()) {
+            glfwPollEvents();
+            drawFrame();
+        }
+        vkDeviceWaitIdle(lotDevice.device());        
+    }
+
+    void FirstApp::loadModels() {
+        std::vector<LotModel::Vertex> vertices {
+            {{  0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
+            {{  0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f }},
+            {{ -0.5f,  0.5f }, { 0.0f, 0.0f, 1.0f }}
+        };
+        lotModel = std::make_unique<LotModel>(lotDevice, vertices);
+    }
+
+    void FirstApp::createPipelineLayout() {
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;
+        pipelineLayoutInfo.pSetLayouts = nullptr;
+        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        if (vkCreatePipelineLayout(lotDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create pipeline layout");
+        }
+    }
+
+    void FirstApp::createPipeline() {
+        PipelineConfigInfo pipelineConfig {};
+        LotPipeline::defaultPipelineConfigInfo(pipelineConfig, lotSwapChain.width(), lotSwapChain.height());
+        pipelineConfig.renderPass = lotSwapChain.getRenderPass();
+        pipelineConfig.pipelineLayout = pipelineLayout;
+        lotPipeline = std::make_unique<LotPipeline>(
+            lotDevice, 
+            "shaders/simple_shader.vert.spv",
+            "shaders/simple_shader.frag.spv", 
+            pipelineConfig);
+    }
+
+    void FirstApp::createCommandBuffers() {
+        commandBuffers.resize(lotSwapChain.imageCount());
+
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = lotDevice.getCommandPool();
+        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+        if (vkAllocateCommandBuffers(lotDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffers");
+        }
+
+        for (int i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = lotSwapChain.getRenderPass();
+            renderPassInfo.framebuffer = lotSwapChain.getFrameBuffer(i);
+
+            renderPassInfo.renderArea.offset = { 0, 0 };
+            renderPassInfo.renderArea.extent = lotSwapChain.getSwapChainExtent();
+
+            std::array<VkClearValue ,2> clearValues{};
+            clearValues[0].color = { 1.0f, 0.1f, 0.1f };
+            clearValues[1].depthStencil = { 1.0f, 0 };
+            renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
+
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+            lotPipeline->bind(commandBuffers[i]);
+            lotModel->bind(commandBuffers[i]);
+            lotModel->draw(commandBuffers[i]);
+
+            vkCmdEndRenderPass(commandBuffers[i]);
+            if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to record command buffer!");
+            }
+        }
+    }
+
+    void FirstApp::drawFrame() {
+        uint32_t imageIndex;
+        auto result = lotSwapChain.acquireNextImage(&imageIndex);
+
+        if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+            throw std::runtime_error("failed to acquire swap chain image!");
+        }
+
+        result = lotSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to present swap chain image!");
+        }
+    }
+} // namespce lot
